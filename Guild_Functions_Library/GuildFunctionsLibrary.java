b1cbb68952623a83b899cb0eeb4b7d6cb4bbacc0
Sediel17 <xzedxx17@gmail.com>
2026-03-26 21:03:59 -0500

    mi progreso antes de crear otra rama

---
+++ Guild_Functions_Library/GuildFunctionsLibrary.java
@@ -0,0 +1,21 @@
+public class Guild_Functions_Library {
+    public static double sum(double[] 
+numbers) {
+    double total = 0;
+    for (int i = 0; i < 
+        numbers.lenght; i++)
+        total +=numbers [i];
+    }
+    return total;
+    }
+public static double average(double[]
+    numbers) {
+        return sum(numbers) /
+    numbers.length;
+    }
+    public static double max(double[]
+        numbers) {
+            doubke maxValue = numbers[0];
+
+}
+
